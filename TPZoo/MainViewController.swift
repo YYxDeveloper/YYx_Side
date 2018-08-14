a4d26f6caac0a29b9b88db060cd8bd7de47bc4f1
yyxdev <yyxdev@gmail.com>
2018-08-14 18:26:20 +0800

    CLLocationManagerDelegate

--- TPZoo/MainViewController.swift
+++ TPZoo/MainViewController.swift
@@ -10,35 +10,48 @@ import UIKit
 import CoreData
 import GoogleMaps
 
-class MainViewController: UIViewController {
 
+class MainViewController: UIViewController, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation?
+    var mapView: GMSMapView!
+    var zoomLevel: Float = 15.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
-        let mapView = GoogleMapManager.newZooMapView()
-        view = mapView
-        GoogleMapManager.addGMSMarker(with: mapView, cordinate: CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20))
+        
+      
+//        GoogleMapManager.shared.addGMSMarker(cordinate: CLLocationCoordinate2D(latitude: 24.997134, longitude: 121.585503))
         //test
     
 //        print(aa)
-
+        
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        
+        locationManager = GoogleMapManager.shared.locationManager
+        locationManager.delegate = self
+        self.mapView = GoogleMapManager.shared.mapView
+        view = self.mapView
+        
+       
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations.last
+        
+//        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude:(location?.coordinate.longitude)!, zoom:zoomLevel)
+//        mapView.animate(to: camera)
+        
+        //Finally stop updating location otherwise it will come again and again in this delegate
+        self.locationManager.stopUpdatingLocation()
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
